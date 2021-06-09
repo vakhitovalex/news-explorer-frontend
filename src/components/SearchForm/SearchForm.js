1423af05314c249fa4b4bda99725c3916e13cba9
Alex Vakhitov <vakhitovalex@gmail.com>
2021-06-09 08:33:04 -0700

    searchform wip

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './SearchForm.css';
 
 function SearchForm(props) {
+  const [searchKeyword, setSearchKeyword] = useState('');
+  function handleSearchKeywordChange(e) {
+    setSearchKeyword(e.target.value);
+  }
+
   return (
     <form action='' className='searchform'>
-      <input className='searchform__input' placeholder='Nature'></input>
+      <input
+        className='searchform__input'
+        placeholder='Nature'
+        value={searchKeyword}
+      ></input>
       <button className='searchform__submit' type='submit'>
         Search
       </button>
