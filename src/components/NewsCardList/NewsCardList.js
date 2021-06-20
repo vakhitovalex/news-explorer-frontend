a01bc0464f0471ee96002904836ccacbb20adf35
Alex Vakhitov <vakhitovalex@gmail.com>
2021-06-20 14:30:54 -0700

    search block show no results if nothing found

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -6,39 +6,39 @@ import notFound from '../../images/not-found.svg';
 import NewsCard from '../NewsCard/NewsCard';
 
 function NewsCardList(props) {
-  console.log(props.newsCards);
+  console.log(props.newsCards.length);
   return (
     <section className='newscards'>
       <h2 className={NewsCard ? 'newscards__title' : undefined}>
         Search results
       </h2>
-      <ul className='newscards__list'>
-        {props.newsCards.map((newsCard) => (
-          <NewsCard
-            key={newsCard.url}
-            newsCard={newsCard}
-            searchKeyword={props.searchKeyword}
-          />
-        ))}
-        {/* <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard /> */}
-      </ul>
-      <button className='newscards__expand'>Show more</button>
-      <div className='results'>
-        <img
-          className='results__image'
-          alt='nothing found'
-          src={notFound}
-        ></img>
-        <h3 className='results__title'>Nothing Found</h3>
-        <h4 className='results__subtitle'>
-          Sorry, but nothing matched your search terms.
-        </h4>
-      </div>
+      {props.newsCards.length > 0 ? (
+        <ul className='newscards__list'>
+          {props.newsCards.map((newsCard) => (
+            <NewsCard
+              key={newsCard.url}
+              newsCard={newsCard}
+              searchKeyword={props.searchKeyword}
+            />
+          ))}
+        </ul>
+      ) : (
+        <div className='results'>
+          <img
+            className='results__image'
+            alt='nothing found'
+            src={notFound}
+          ></img>
+          <h3 className='results__title'>Nothing Found</h3>
+          <h4 className='results__subtitle'>
+            Sorry, but nothing matched your search terms.
+          </h4>
+        </div>
+      )}
     </section>
   );
 }
-
+{
+  /* <button className='newscards__expand'>Show more</button>; */
+}
 export default NewsCardList;
