cc48c5372a25638e422a2ca52f13f09593f260d7
Alex Vakhitov <vakhitovalex@gmail.com>
2021-06-21 15:31:25 -0700

    add show more button functionality

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NewsCardList.css';
 import notFound from '../../images/not-found.svg';
@@ -6,15 +6,33 @@ import notFound from '../../images/not-found.svg';
 import NewsCard from '../NewsCard/NewsCard';
 
 function NewsCardList(props) {
-  console.log(props.newsCards.length);
+  const [index, setIndex] = useState(1);
+
+  const chunk = (arr, chunkSize = 1, cache = []) => {
+    const tmp = [...arr];
+    if (chunkSize <= 0) return cache;
+    while (tmp.length) cache.push(tmp.splice(0, chunkSize));
+    return cache;
+  };
+
+  const newsChunks = chunk(props.newsCards, 3 * index);
+
+  const expandRows = () => {
+    setIndex(index + 1);
+  };
+  console.log(newsChunks);
+
+  console.log(newsChunks.length);
+
+  // console.log(props.newsCards.length);
   return (
     <section className='newscards'>
       <h2 className={NewsCard ? 'newscards__title' : undefined}>
         Search results
       </h2>
-      {props.newsCards.length > 0 ? (
+      {newsChunks ? (
         <ul className='newscards__list'>
-          {props.newsCards.map((newsCard) => (
+          {newsChunks[0].map((newsCard) => (
             <NewsCard
               key={newsCard.url}
               newsCard={newsCard}
@@ -24,21 +42,56 @@ function NewsCardList(props) {
         </ul>
       ) : (
         <div className='results'>
+          {' '}
           <img
             className='results__image'
             alt='nothing found'
             src={notFound}
           ></img>
-          <h3 className='results__title'>Nothing Found</h3>
+          <h3 className='results__title'>Nothing Found</h3>{' '}
           <h4 className='results__subtitle'>
-            Sorry, but nothing matched your search terms.
-          </h4>
+            Sorry, but nothing matched your search terms.{' '}
+          </h4>{' '}
         </div>
+      )}
+      {newsChunks.length > 1 ? (
+        <button className='newscards__expand' onClick={expandRows}>
+          Show more
+        </button>
+      ) : (
+        ''
       )}
     </section>
   );
 }
-{
-  /* <button className='newscards__expand'>Show more</button>; */
-}
+
 export default NewsCardList;
+
+//  {
+//    props.newsCards.length > 0 ? (
+//      <ul className='newscards__list'>
+//        {props.newsCards.map((newsCard) => (
+//          <NewsCard
+//            key={newsCard.url}
+//            newsCard={newsCard}
+//            searchKeyword={props.searchKeyword}
+//          />
+//        ))}
+//      </ul>
+//    ) : (
+//      <div className='results'>
+//        <img className='results__image' alt='nothing found' src={notFound}></img>
+//        <h3 className='results__title'>Nothing Found</h3>
+//        <h4 className='results__subtitle'>
+//          Sorry, but nothing matched your search terms.
+//        </h4>
+//      </div>
+//    );
+//  }
+//  {
+//    props.newsCards.length > 3 ? (
+//      <button className='newscards__expand'>Show more</button>
+//    ) : (
+//      ''
+//    );
+//  }
